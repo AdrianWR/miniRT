0d832da7a0c0af794e15ac9b448b2641a05326ac
AdrianWR <adrian.w.roque@gmail.com>
2020-07-09 17:31:50 -0300

    Add point type definition

--- include/vector.h
+++ include/vector.h
@@ -6,7 +6,7 @@
 /*   By: aroque <aroque@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/14 15:56:30 by aroque            #+#    #+#             */
-/*   Updated: 2020/05/15 11:15:09 by aroque           ###   ########.fr       */
+/*   Updated: 2020/07/08 10:16:31 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ typedef struct		s_vector
 typedef	t_vector	t_point;
 
 t_vector			vector(float x, float y, float z);
+t_point				point(float x, float y, float z);
 t_vector			add(t_vector u, t_vector v);
 t_vector			sub(t_vector u, t_vector v);
 t_vector			scale(t_vector u, float c);
--- src/vector.c
+++ src/vector.c
@@ -6,13 +6,13 @@
 /*   By: aroque <aroque@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/14 16:01:16 by aroque            #+#    #+#             */
-/*   Updated: 2020/05/14 16:02:15 by aroque           ###   ########.fr       */
+/*   Updated: 2020/07/08 10:16:53 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector.h"
 
-t_vector		vector(float x, float y, float z)
+t_vector	vector(float x, float y, float z)
 {
 	t_vector v;
 
@@ -20,4 +20,9 @@ t_vector		vector(float x, float y, float z)
 	v.y = y;
 	v.z = z;
 	return (v);
+}
+
+t_point		point(float x, float y, float z)
+{
+	return (vector(x, y, z));
 }
