95e73221162712d4f372bc6acac0059884286659
AdrianWR <adrian.w.roque@gmail.com>
2020-09-13 20:28:39 -0300

    Remove hashtable library

--- lib/libft/test/minunit.h
+++
@@ -1,9 +0,0 @@
-/*
-** This unit test framework can be found at
-** http://www.jera.com/techinfo/jtns/jtn002.html
-** All credits to its creator.
-*/
-
-#define mu_assert(message, test) do { if (!(test)) return message; } while (0)
-#define mu_run_test(test) do { char *message = test(); tests_run++; if (message) return message; } while (0)
-extern int tests_run;
